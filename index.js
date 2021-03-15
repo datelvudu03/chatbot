71f1fecc7f369fd1c35935ebecb91a0272c216bf
Tranda <datelvudy03@seznam.cz>
2021-03-15 19:36:23 +0100

    Zatim final

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const BootBot = require('bootbot');
 const config = require('config');
 const fetch = require('node-fetch');
-const CoinGecko = require('coingecko-api');
 var emoji = require('node-emoji');
 
 // yarn startTunnel
@@ -16,17 +15,19 @@ const bot = new BootBot({
 
 // variable
 var currency = 'usd';
-var watchedPercentP = 1;
-var watchedPercentM = -1;
-var timer = 300000;
+var watchedPercentP = 3;
+var watchedPercentM = -3;
+var dogTimer = 10000;
 var watchDogP = 5;
-var userId,watchDog;
+var userId, watchDogC, dogTimeInt;
 
 
 bot.setGreetingText('Hey there! Welcome to Cryptocheck!');
 
 bot.setGetStartedButton((payload, chat) => {
   propMenu(chat, true);
+  userId = payload.sender.id;
+  console.log(userId)
 });
 
 // payload = obsah chatu; chat == konkretni chat
@@ -42,9 +43,14 @@ bot.on('postback:SET_CURRENCY', (payload, chat) => {
 });
 
 
-bot.on('postback:SET_PERCENT', (payload, chat) => {
-  chat.say(`The current percent is "${watchedPercentP}" %`);
-  chat.say('Pls write "setper" and then your wanted currency for example( czk, usd, eur, btc, eth, etc.) ');
+bot.on('postback:SET_PPERCENT', (payload, chat) => {
+  chat.say(`The current percentage is "${watchedPercentP}" %`);
+  chat.say('Pls write "setpper" and then your wanted precentage for example(setpper1, setpper 3, etc. ) ');
+});
+
+bot.on('postback:SET_MPERCENT', (payload, chat) => {
+  chat.say(`The current percentage is "${watchedPercentM}" %`);
+  chat.say('Pls write "setmper" and then your wanted precentage for example(setmper -1,setmper -3, etc. ) ');
 });
 
 bot.on('postback:Q_CHECK', (payload, chat) => {
@@ -56,9 +62,28 @@ bot.on('postback:PRICE_CHECK', (payload, chat) => {
 })
 
 bot.on('postback:PC_CHECK', (payload, chat) => {
+  propPcCheck(chat)
+})
+
+bot.on('postback:TOP_WINNERS', (payload, chat) => {
   checkPriceChange(currency, watchedPercentP, watchedPercentM).then(result => {
-    chat.say(result[0]);
-    chat.say(result[1]);
+    if(result[0] == null){
+      chat.say(`There is no coin that has more then ${watchedPercentP}% change.`);
+    } else {
+      chat.say(result[0]);
+    }
+    
+  });
+})
+
+bot.on('postback:TOP_LOSERS', (payload, chat) => {
+  checkPriceChange(currency, watchedPercentP, watchedPercentM).then(result => {
+  
+    if(result[1] == null){
+      chat.say(`There is no coin that has less then ${watchedPercentM}% change.`);
+    } else {
+      chat.say(result[1]);
+    }
   });
 })
 
@@ -70,33 +95,92 @@ bot.on('postback:SETTING', (payload, chat) => {
   propSetting(chat)
 })
 
-bot.on('postback:WATCHDOG', (payload, chat) => {
-  if (watchDog == null) {
+bot.on('postback:WATCH_DOG_SET', (payload, chat) => {
+  watchDogInit(chat)
+})
+
+bot.on('postback:SET_WATCHDOGP', (payload, chat) => {
+  chat.say(`The current percentage is "${watchDogP}" %`);
+  chat.say('Pls write "setWDper" and then your wanted precentage for example(1, 5, 10, etc. ) ');
+})
+
+function watchDogInit(chat) {
+  chat.say({
+    text: 'Watch dog settings',
+    buttons: [
+      { type: 'postback', title: 'Change watched coin', payload: 'WATCH_DOG' },
+      { type: 'postback', title: 'Change timer', payload: 'SET_TIMER' },
+      { type: 'postback', title: 'Change watched %', payload: 'SET_WATCHDOGP' }
+    ]
+  })
+}
+
+bot.on('postback:SET_TIMER', (payload, chat) => {
+  chat.say(`Timer is set for ${dogTimer / 60000} minutes.`)
+  chat.say('Pls type "setdtimer" + time that you wish to watch.\nExample: setdtimer 15.\nTimer can be set just in MINUTES.\nTimer can be set for a least 5 minutes.')
+})
+
+bot.on('postback:SET_TOP7', (payload, chat) => {
+  propChangeTop7P(chat);
+})
+
+bot.on('postback:TURN_ON', (payload, chat) => {
+  if (watchDogC != null) {
+    userId = payload.sender.id;
+    clearInterval(dogTimeInt);
+    chat.say(`Dog is watching ${watchDogC}.\nDoggo will check every ${dogTimer / 60000} minutes.\nIf the price changes more then ${watchDogP} % doggo will notify you. `);
+    dogTimeInt = setInterval(dogWatch, dogTimer);
+  } else {
+    chat.say(`Doggo has no coin to watch.\nPls write "setdog" and then your wanted coin.`)
+  }
+
+})
+
+bot.on('postback:TURN_OFF', (payload, chat) => {
+  clearInterval(dogTimeInt);
+  chat.say("Doggo went sleep.")
+
+})
+
+bot.on('postback:WATCH_DOG', (payload, chat) => {
+  if (watchDogC == null) {
+    chat.say('There is no coin for doggo to lookout.')
     chat.say('Pls write "setdog" and then your wanted coin for example( bitcoin, cardano, etherium, etc.) ');
   } else {
-    userId = payload.sender.id;
-    setInterval(dogWatch,5000);
+    propWatchDog(chat);
   }
-  
+
 });
 
-async function dogWatch () {
+function propWatchDog(chat) {
+  chat.say({
+    text: 'Watch Dog',
+    buttons: [
+      { type: 'postback', title: 'Turn on', payload: 'TURN_ON' },
+      { type: 'postback', title: 'Turn off', payload: 'TURN_OFF' }
+    ]
+  })
 
-var link = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${watchDog}&order=market_cap_desc&per_page=1&page=1&sparkline=false&price_change_percentage=1h`  
-var response = await fetch(link);
-response = await response.json();
-
-console.log(response[0].price_change_percentage_1h_in_currency >= watchDogP)
-if (response[0].price_change_percentage_1h_in_currency >= watchDogP) {
-  var text = `${response[0].name} raises to ${response[0].current_price}.\nIts current price is ${response[0].price_change_percentage_1h_in_currency} higher from 1 Hour ago.`
-  bot.say(userId,text)
-}
 
 }
+async function dogWatch() {
+  var link = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${watchDogC}&order=market_cap_desc&per_page=1&page=1&sparkline=false&price_change_percentage=1h`
+  var response = await fetch(link);
+  response = await response.json();
+  console.log(response[0].name)
+
+  console.log(response[0].price_change_percentage_1h_in_currency >= watchDogP)
+  if (response[0].price_change_percentage_1h_in_currency >= watchDogP) {
+    var text = `${response[0].name} raises to ${response[0].current_price}.\nIts current price is ${response[0].price_change_percentage_1h_in_currency} higher from 1 Hour ago.`
+    bot.say(userId, text)
+  }
+
+}
+
 
 bot.hear(/qcheck (.*)/i, (payload, chat, data) => {
   isSupported(data.match[1]).then(result => {
-    if (result == true) {
+    if (result) {
       checkSpCoin(data.match[1]).then(result => chat.say(result))
     } else {
       chat.say(`Coin ${data.match[1]} is not supported.`)
@@ -118,21 +202,63 @@ bot.hear(/setcur (.*)/i, (payload, chat, data) => {
     }
   })
 });
-bot.hear(/setper (.*)/i, (payload, chat, data) => {
+
+bot.hear(/setWDper (.*)/i, (payload, chat, data) => {
 
   var x = parseInt(data.match[1], 10)
   if (Number.isInteger(x)) {
-    watchedPercentP = x;
-    chat.say(`Change percent to ${watchedPercentP}%`)
+    var temp = watchDogP;
+    watchDogP = x;
+    chat.say(`Change from ${temp}% to ${watchDogP}%`)
   }
 
   else { chat.say(`${data.match[1]} is not a number.`) }
 
 });
+
+bot.hear(/setpper (.*)/i, (payload, chat, data) => {
+
+  var x = parseInt(data.match[1], 10)
+  if (Number.isInteger(x)) {
+    if (x > 0) {
+      var temp = watchedPercentP;
+      watchedPercentP = x;
+      chat.say(`Change from ${temp}% to ${watchedPercentP}%`)
+    } else {
+      chat.say(`Percetage has to greater than 0.\n You typed ${x}%`)
+    }
+
+  }
+
+  else { chat.say(`${data.match[1]} is not a number.`) }
+
+});
+
+bot.hear(/setmper (.*)/i, (payload, chat, data) => {
+
+  var x = parseInt(data.match[1], 10)
+  if (Number.isInteger(x)) {
+    if (x < 0) {
+      var temp = watchedPercentM;
+      watchedPercentM = x;
+      chat.say(`Change from ${temp}% to ${watchedPercentM}%`)
+    } else {
+      chat.say(`Percetage has to less than 0.\n You typed ${x}%`)
+    }
+
+  }
+
+  else { chat.say(`${data.match[1]} is not a number.`) }
+
+});
+
+
 bot.hear(/setdog (.*)/i, (payload, chat, data) => {
   isSupported(data.match[1]).then(result => {
     if (result) {
-      watchDog = data.match[1].toLowerCase().replace(/ /g, "-");
+      watchDogC = data.match[1].toLowerCase().replace(/ /g, "-");
+      chat.say(`Dog will watch ${data.match[1]}.`);
+      propWatchDog(chat)
     } else {
       chat.say(`${data.match[1]} is not supported.`)
     }
@@ -143,11 +269,15 @@ bot.hear(/setdog (.*)/i, (payload, chat, data) => {
 bot.hear(/setdtimer (.*)/i, (payload, chat, data) => {
   var time = parseInt(data.match[1], 10)
   if (Number.isInteger(time)) {
-    time = time*60000
-    if(time <= timer ) {
+    time = time * 60000
+    if (time < dogTimer) {
       chat.say(`Timer can only be greater then 5 minutes.`)
-    } else {
-      timer = time;
+    } else if( time == dogTimer) {
+      chat.say(`Timer is already set for ${dogTimer / 60000} minutes.`)
+    }
+    else {
+      dogTimer = time;
+      chat.say(`Timer is set for ${dogTimer / 60000} minutes.`)
     }
   }
   else { chat.say(`${data.match[1]} is not a number.`) }
@@ -163,15 +293,18 @@ bot.hear(['---'], (payload, chat) => {
 
 bot.hear(['menu'], (payload, chat) => {
   propMenu(chat, false)
+  console.log(chat)
+  setTimeout(propSetting, 500,chat);
+ 
 });
 
-bot.hear(['setting'], (payload,chat) => {
+bot.hear(['setting'], (payload, chat) => {
   chat.say({
     text: 'What do you need help with?',
     buttons: [
-      { type: 'postback', title: 'Set currency', payload: 'SET_CURRENCY' },
-      { type: 'postback', title: 'Set percent', payload: 'SET_PERCENT' }
-    ]
+      { type: 'postback', title: 'Currency', payload: 'SET_CURRENCY' },
+      { type: 'postback', title: 'Top7 %', payload: 'SET_TOP7' },
+      { type: 'postback', title: 'Watch Dog', payload: 'WATCH_DOG_SET' }]
   });
 });
 
@@ -213,16 +346,17 @@ function propMenu(chat, gettingStarted) {
 
       { type: 'postback', title: 'Price check', payload: 'PRICE_CHECK' },
       { type: 'postback', title: 'Top7 Change', payload: 'PC_CHECK' },
-      { type: 'postback', title: 'Watch Dog', payload: 'WATCHDOG' }
+      { type: 'postback', title: 'Watch Dog', payload: 'WATCH_DOG' }
 
     ]
   });
+}
+function propChangeTop7P(chat) {
   chat.say({
-    text: "Setting:",
+    text: 'Top 7',
     buttons: [
-      { type: 'postback', title: 'Set currency', payload: 'SET_CURRENCY' },
-      { type: 'postback', title: 'Set percent', payload: 'SET_PERCENT' },
-      
+      { type: 'postback', title: 'Set % for winners', payload: 'SET_PPERCENT' },
+      { type: 'postback', title: 'Set % for losers', payload: 'SET_MPERCENT' }
     ]
   });
 }
@@ -237,12 +371,24 @@ function propPriceCheck(chat) {
   });
 }
 
+function propPcCheck(chat) {
+  chat.say({
+    text: 'Top 7',
+    buttons: [
+      { type: 'postback', title: 'Top winner', payload: 'TOP_WINNERS' },
+      { type: 'postback', title: 'Top losers', payload: 'TOP_LOSERS' }
+    ]
+  });
+}
+
 function propSetting(chat) {
   chat.say({
     text: 'Setting',
     buttons: [
-      { type: 'postback', title: 'Set currency', payload: 'SET_CURRENCY' },
-      { type: 'postback', title: 'Set percent', payload: 'SET_PERCENT' }
+      { type: 'postback', title: 'Currency', payload: 'SET_CURRENCY' },
+      { type: 'postback', title: 'Top7 %', payload: 'SET_TOP7' },
+      { type: 'postback', title: 'Watch Dog', payload: 'WATCH_DOG_SET' }
+
     ]
   });
 }
@@ -261,15 +407,18 @@ function convertUnixToTime(timestamp) {
   return time;
 }
 
+function roundUp(num) {
+  return (Math.round(num * 100)/100).toFixed(2)
+}
 
 async function checkPriceChange(currency, vauleP, valueM) {
   const link = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h`
   let response = await fetch(link);
   response = await response.json();
   var arrayEmoji = ["one", "two", "three", "four", "five", "six", "seven"]
-  var textP = "In Plus \n";
-  var textM = "In Minus \n";
-  var arrayInfo = ["Name:", "Price:", "1H %-change:"];
+  var textP = `Current watched %: ${watchedPercentP}\n`;
+  var textM = `Current watched %: ${watchedPercentM}\n`;
+  var arrayInfo = ["Name:", "Price:", "1H change:"];
   var arrayMainP = [];
   var arrayMainM = [];
 
@@ -338,11 +487,19 @@ async function checkPriceChange(currency, vauleP, valueM) {
   }
 
   for (x in arrayMainP) {
-    textP += `${emoji.get(arrayEmoji[x])}\n${arrayInfo[0]} ${arrayMainP[x][0]}\n${arrayInfo[1]} ${arrayMainP[x][1]}\n${arrayInfo[2]} ${arrayMainP[x][2]}\n`
+    textP += `${emoji.get(arrayEmoji[x])}\n${arrayInfo[0]} ${arrayMainP[x][0]}\n${arrayInfo[1]} ${arrayMainP[x][1]} ${currency}\n${arrayInfo[2]} ${roundUp(arrayMainP[x][2])} %\n`
   }
 
   for (x in arrayMainM) {
-    textM += `${emoji.get(arrayEmoji[x])}\n${arrayInfo[0]} ${arrayMainM[x][0]}\n${arrayInfo[1]} ${arrayMainM[x][1]}\n${arrayInfo[2]} ${arrayMainM[x][2]}\n`
+    textM += `${emoji.get(arrayEmoji[x])}\n${arrayInfo[0]} ${arrayMainM[x][0]}\n${arrayInfo[1]} ${arrayMainM[x][1]} ${currency}\n${arrayInfo[2]} ${roundUp(arrayMainM[x][2])} %\n`
+  }
+  
+  if(arrayMainP.length == 0) {
+    textP = null
+  }
+
+  if(arrayMainM.length == 0) {
+    textM = null
   }
   var arrayEnd = [textP, textM]
   return arrayEnd
@@ -388,19 +545,19 @@ async function checkDCoin(coin) {
     coinName = response.name,
     price = `${response.market_data.current_price[`${currency}`]} ${currency.toUpperCase()}`,
     ath = `${response.market_data.ath[`${currency}`]} ${currency.toUpperCase()}`,
-    athChange = `${response.market_data.ath_change_percentage[`${currency}`].toFixed(2)} %`,
+    athChange = `${roundUp(response.market_data.ath_change_percentage[`${currency}`])} %`,
     atl = `${response.market_data.atl[`${currency}`]} ${currency.toUpperCase()}`,
-    atlChange = `${response.market_data.atl_change_percentage[`${currency}`].toFixed(2)} %`,
+    atlChange = `${roundUp(response.market_data.atl_change_percentage[`${currency}`])} %`,
     totalVolume = `${response.market_data.total_volume[`${currency}`]} ${currency.toUpperCase()}`,
     high24 = `${response.market_data.high_24h[`${currency}`]} ${currency.toUpperCase()}`,
     low24 = `${response.market_data.low_24h[`${currency}`]} ${currency.toUpperCase()}`,
     price24Change = `${response.market_data.price_change_24h_in_currency[`${currency}`]} ${currency.toUpperCase()}`,
-    price1hPchange = `${response.market_data.price_change_percentage_1h_in_currency[`${currency}`].toFixed(2)} %`,
-    price24hPchange = `${response.market_data.price_change_percentage_24h_in_currency[`${currency}`].toFixed(2)} %`,
-    price7dPchange = `${response.market_data.price_change_percentage_7d_in_currency[`${currency}`].toFixed(2)} %`,
-    price14dPchange = `${response.market_data.price_change_percentage_14d_in_currency[`${currency}`].toFixed(2)} %`,
-    price30dPchange = `${response.market_data.price_change_percentage_30d_in_currency[`${currency}`].toFixed(2)} %`,
-    price1yPchange = `${response.market_data.price_change_percentage_1y_in_currency[`${currency}`].toFixed(2)} %`
+    price1hPchange = `${roundUp(response.market_data.price_change_percentage_1h_in_currency[`${currency}`])} %`,
+    price24hPchange = `${roundUp(response.market_data.price_change_percentage_24h_in_currency[`${currency}`])} %`,
+    price7dPchange = `${roundUp(response.market_data.price_change_percentage_7d_in_currency[`${currency}`])} %`,
+    price14dPchange = `${roundUp(response.market_data.price_change_percentage_14d_in_currency[`${currency}`])} %`,
+    price30dPchange = `${roundUp(response.market_data.price_change_percentage_30d_in_currency[`${currency}`])} %`,
+    price1yPchange = `${roundUp(response.market_data.price_change_percentage_1y_in_currency[`${currency}`])} %`
   ]
   var arrayName = ["Name:", "Price:", "All-time high:", "Change from all-time high:", "All-time low:", "Change from all-time low:", "Traded volume:", "24-High:", "24-Low:"
     , "24H change:", "1H %-change:", "24H %-change:", "7D %-change:", "14D %-change:", "30D %-change:", "1Y %-change:"];
@@ -410,7 +567,16 @@ async function checkDCoin(coin) {
   return finalArray
 
 }
-
+tryMe();
+async function tryMe() {
+  var asd = true;
+  while(asd) {
+    var ds = await setInterval(try2,3000)
+  }
+}
+function try2() {
+  console.log(asd)
+}
 async function checkSpCoin(coin) {
   const simplePrice = `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${currency}&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true`
   let response = await fetch(simplePrice);
@@ -420,7 +586,7 @@ async function checkSpCoin(coin) {
   var vol24h = response[`${coin}`][`${currency}_24h_vol`];
   var change24h = response[`${coin}`][`${currency}_24h_change`];
   var lastUpdatedAt = convertUnixToTime(response[`${coin}`].last_updated_at);
-  let text = `Price: ${price} ${currency.toUpperCase()}\nMarket cap: ${marketCap}\n24h volume: ${vol24h}\n24h change: ${change24h}\nLast updated: ${lastUpdatedAt}`;
+  let text = `Price: ${price} ${currency.toUpperCase()}\nMarket cap: ${roundUp(marketCap)}\n24h volume: ${roundUp(vol24h)}\n24h change: ${roundUp(change24h)}\nLast updated: ${lastUpdatedAt}`;
   return text;
 }
 bot.start(port);
